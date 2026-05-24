9d1c71970443fec136aa77995d2c63aed885be30
AngelRuiiz12 <angel.rs99@hotmail.com>
2026-05-24 13:38:09 +0200

    Añado colisiones

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -40,6 +42,13 @@ def main():
         screen.fill("black")
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         # Dibujamos el triangulo del player
         for item in drawable:
             item.draw(screen)
