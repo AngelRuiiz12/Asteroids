760864421ca3ccb82241e32cc9e8dcf557100680
AngelRuiiz12 <angel.rs99@hotmail.com>
2026-05-25 19:13:27 +0200

    Añado interacción entre balas y asteroides

--- main.py
+++ main.py
@@ -53,6 +53,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
         # Dibujamos el triangulo del player
         for item in drawable:
             item.draw(screen)
