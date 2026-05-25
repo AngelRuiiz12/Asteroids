21eaaec4fce6968adb68c751e11996151a26ca8f
AngelRuiiz12 <angel.rs99@hotmail.com>
2026-05-25 19:25:45 +0200

    Hago que los asteroides se dividan en dos al morir

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt) -> None:
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_new_asteroid_vector = self.velocity.rotate(angle)
+        second_new_asteroid_vector = self.velocity.rotate(-angle)
+
+        first_new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+        second_new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+
+        first_new_asteroid.velocity = first_new_asteroid_vector * 1.2
+        second_new_asteroid.velocity = second_new_asteroid_vector * 1.2
